379b4cde63ce68b0a0880f5905b7da1d48da2c46
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 15:03:30 -0300

    Add project files.

---
+++ ExercicioDois.ConsoleApp/Graus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDois.ConsoleApp
+{
+    internal class Graus
+    {
+        public double fahrenheit;
+        public double celsius;
+        public double ConverterFahrenheitCelsius(double fahrenheit)
+        {
+            celsius = (fahrenheit - 32) / 1.8;
+            return celsius;
+        }
+    }
+}
---
+++ ExercicioDois.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioDois.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graus fah = new Graus();
+            Console.WriteLine("Informa a temperatura em ºF: \n> ");
+            fah.fahrenheit = double.Parse(Console.ReadLine());
+            fah.celsius = fah.ConverterFahrenheitCelsius(fah.fahrenheit);
+            Console.Clear();
+            Console.WriteLine(fah.fahrenheit + "°F, convertidos ficam " + fah.celsius.ToString("#.##") + "°C.");
+            Console.ReadKey();
+        }
+    }
+}
